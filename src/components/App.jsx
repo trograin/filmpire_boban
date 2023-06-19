e05fb324fec913c25deb9da6c49d32ceaea5fc12
boban.alempijevic <boban.alempijevic@gmail.com>
2023-06-19 10:00:46 +0300

    added full component structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+
+// / -> root -> all movies
+// /asd123 -> movie information -> more
 
 const App = () => (
-    <div>App</div>
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Switch>
+        <Route exact path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>      
+    </div>
   );
 
 export default App
