037bd19853458668038fc4c0a36239e3b4233f19
boban.alempijevic <boban.alempijevic@gmail.com>
2023-06-27 12:09:28 +0300

    fetching movie data and displaying titles

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,13 +3,37 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import  MovieList  from "../MovieList/MovieList";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if(isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" >
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if(!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px" >
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if(error) return 'An error has occured';
 
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data}/>
+    </div>
   )
 }
 
