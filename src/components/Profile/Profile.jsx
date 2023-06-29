eae88a7f42f8d50907fc3249b8e01279b5cd2997
boban.alempijevic <boban.alempijevic@gmail.com>
2023-06-29 13:02:33 +0300

    Added Logout functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
   const { user } =useSelector(userSelector);
-    console.log(user);
+  const favoriteMovies = [];
+    
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+
   return (
-    <div>
-      Profile - {user.username}
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            Favorite movies
+          </Box>
+        )}
+    </Box>
   )
 }
 
