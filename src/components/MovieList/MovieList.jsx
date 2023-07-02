881c0bcf261bf08291b0d7cf53cb0dd43dfbc643
boban.alempijevic <boban.alempijevic@gmail.com>
2023-07-02 07:26:24 +0300

    Movieinformation added

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,17 +4,17 @@ import { Grid } from '@mui/material';
 import useStyles from './styles';
 import { Movie } from '..';
 
+const MovieList = ({ movies, numberOfMovies, excludeFirst }) => {
+  const classes = useStyles();
+  const startFrom = excludeFirst ? 1 : 0;
 
-const MovieList = ({ movies }) => {
-    const classes = useStyles();
-
-    return (
-        <Grid container className={classes.moviesContainer}>
-            {movies.results.map((movie, i) => (
-                <Movie key={i} movie={movie} i={i} />
-            ))}
-        </Grid>
-    );
+  return (
+    <Grid container className={classes.moviesContainer}>
+      {movies.results.slice(startFrom, numberOfMovies).map((movie, i) => (
+        <Movie key={i} movie={movie} i={i} />
+      ))}
+    </Grid>
+  );
 };
 
 export default MovieList;
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -33,10 +33,36 @@ export const tmdbApi = createApi({
             return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
             },
         }),
+        //* Get Movie
+        getMovie: builder.query({
+            query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+        }),
+
+        //* Get User Specific Lists
+        getList: builder.query({
+            query: ({ listName, accountId, sessionId, page }) => `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
+        }),
+
+        getRecommendations: builder.query({
+            query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+        }),
+
+        getActorsDetails: builder.query({
+            query: (id) => `/person/${id}?api_key=${tmdbApiKey}`,
+        }),
+
+        getMoviesByActorId: builder.query({
+            query: ({ id, page }) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
+        }),
     }),
 });
 
 export const {
-    useGetGenresQuery,
-    useGetMoviesQuery,
+useGetGenresQuery,
+useGetMoviesQuery,
+useGetMovieQuery,
+useGetListQuery,
+useGetRecommendationsQuery,
+useGetActorsDetailsQuery,
+useGetMoviesByActorIdQuery,
 } = tmdbApi;
