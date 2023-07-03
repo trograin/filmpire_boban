6153b515b42565bf296f64b11748d4e26bc546d4
boban.alempijevic <boban.alempijevic@gmail.com>
2023-07-03 07:17:11 +0300

    Pagination added to Movies and Actors

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetMoviesQuery } from '../../services/TMDB';
-import { MovieList }  from "..";
+import { MovieList, Pagination } from '..';
 
 const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  if(isFetching) {
+  const numberOfMovies = lg ? 17 : 19;
+
+  if (isFetching) {
     return (
-      <Box display="flex" justifyContent="center" >
+      <Box display="flex" justifyContent="center">
         <CircularProgress size="4rem" />
       </Box>
     );
   }
 
-  if(!data.results.length) {
+  if (!data.results.length) {
     return (
-      <Box display="flex" alignItems="center" mt="20px" >
+      <Box display="flex" alignItems="center" mt="20px">
         <Typography variant="h4">
           No movies that match that name.
           <br />
@@ -31,13 +33,14 @@ const Movies = () => {
     );
   }
 
-  if(error) return 'An error has occured';
+  if (error) return 'An error has occured.';
 
   return (
     <div>
-      <MovieList movies={data}/>
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
+      <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
-  )
-}
+  );
+};
 
 export default Movies;
