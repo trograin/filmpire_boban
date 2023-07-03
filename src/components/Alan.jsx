aaae37eaa8fbd531fc9ae7168c41f6a1480e092d
boban.alempijevic <boban.alempijevic@gmail.com>
2023-07-03 21:22:01 +0300

    added Alan Voicecommand to the app with basic functionality

---
+++ src/components/Alan.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import { ColorModeContext } from '../utils/ToggleColorMode';
+
+const useAlan = () => {
+    const { setMode } = useContext(ColorModeContext);
+    useEffect(() => {
+        alanBtn({
+            key: 'e9c2d95a17c6dd414620127cb9d654332e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: ({ command, mode }) => {
+              if (command === 'changeMode') {
+                if (mode === 'light') {
+                    setMode('light');
+                } else {
+                    setMode('dark');
+                }
+              }
+            },
+        });
+      }, []);
+};
+
+export default useAlan;
+
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { ColorModeContext } from '../../utils/ToggleColorMode';
 import { setUser, userSelector } from '../../features/auth';
 import { Sidebar, Search } from '..';
 import { fetchToken, createSessionId, moviesApi } from '../../utils';
@@ -14,10 +15,12 @@ const NavBar = () => {
   const { isAuthenticated, user } = useSelector(userSelector);
   const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
-  const isMobile = useMediaQuery('(max-width:600px)'); //Checks if the screen is larger than 600Px or not
+  const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   const dispatch = useDispatch();
-  
+
+  const colorMode = useContext(ColorModeContext);
+
   const token = localStorage.getItem('request_token');
   const sessionIdFromLocalStorage = localStorage.getItem('session_id');
 
@@ -43,20 +46,20 @@ const NavBar = () => {
 
   return (
     <>
-    <AppBar position="fixed">
-      <Toolbar className={classes.toolbar}>
-        { isMobile && (
-          <IconButton
-            color="inherit"
-            edge="start"
-            style={{ outline: 'none' }}
-            onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
-            className={classes.menuButton}
-          >
-            <Menu />
-          </IconButton>
-        )}
-        <IconButton color="inherit" sx={{ ml:1}} onClick={() => {}}>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}>
             {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
           {!isMobile && <Search />}
@@ -75,40 +78,38 @@ const NavBar = () => {
               >
                 {!isMobile && <>My Movies &nbsp;</>}
                 <Avatar
-                  style={{ width:30, height: 30 }} 
+                  style={{ width: 30, height: 30 }}
                   alt="Profile"
-                  src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw3M1NdJMh3CuLtReHpaEk-K&ust=1687349958618000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICbnuPq0f8CFQAAAAAdAAAAABAE"}                  
-                  />
+                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
+                />
               </Button>
             )}
           </div>
           {isMobile && <Search />}
-      </Toolbar>
-
-    </AppBar>
-    <div>
-      <nav className={classes.drawer}>
-        {isMobile ? (
-          <Drawer 
-          variant='temporary'
-          anchor='right'
-          open={mobileOpen}
-          onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
-          className={classes.drawerBackground}
-          classes={{ paper: classes.drawerPaper}}
-          ModalProps={{ keepMounted: true}}
-          >
-            <Sidebar setMobileOpen={setMobileOpen} />
-          </Drawer>
-        ) : (
-          <Drawer classes={{ paper: classes.drawerPaper}} variant='permanent' open>
-            <Sidebar setMobileOpen={setMobileOpen} />
-          </Drawer>
-        )}
-      </nav>
-    </div>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent" open>
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default NavBar;
